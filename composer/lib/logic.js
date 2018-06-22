78e292b4a9db0400c3533fba63c08c4aab5bf330
cabernety <330785652@qq.com>
2018-06-22 13:01:26 +0800

    11

--- composer/lib/logic.js
+++ composer/lib/logic.js
@@ -16,41 +16,41 @@
 
 /**
  * Track the trade of a commodity from one trader to another
- * @param {org.xuyuntech.health.Trade} trade - the trade to be processed
- * @transaction
+ * @param1 {org.xuyuntech.health.Trade} trade - the trade to be processed
+ * @transaction1
  */
-async function tradeCommodity(trade) {
+// async function tradeCommodity(trade) {
 
-    // set the new owner of the commodity
-    trade.commodity.owner = trade.newOwner;
-    let assetRegistry = await getAssetRegistry('org.xuyuntech.health.Commodity');
+//     // set the new owner of the commodity
+//     trade.commodity.owner = trade.newOwner;
+//     let assetRegistry = await getAssetRegistry('org.xuyuntech.health.Commodity');
 
-    // emit a notification that a trade has occurred
-    let tradeNotification = getFactory().newEvent('org.xuyuntech.health', 'TradeNotification');
-    tradeNotification.commodity = trade.commodity;
-    emit(tradeNotification);
+//     // emit a notification that a trade has occurred
+//     let tradeNotification = getFactory().newEvent('org.xuyuntech.health', 'TradeNotification');
+//     tradeNotification.commodity = trade.commodity;
+//     emit(tradeNotification);
 
-    // persist the state of the commodity
-    await assetRegistry.update(trade.commodity);
-}
+//     // persist the state of the commodity
+//     await assetRegistry.update(trade.commodity);
+// }
 
 /**
  * Remove all high volume commodities
- * @param {org.xuyuntech.health.RemoveHighQuantityCommodities} remove - the remove to be processed
- * @transaction
+ * @param1 {org.xuyuntech.health.RemoveHighQuantityCommodities} remove - the remove to be processed
+ * @transaction1
  */
-async function removeHighQuantityCommodities(remove) {
+// async function removeHighQuantityCommodities(remove) {
 
-    let assetRegistry = await getAssetRegistry('org.xuyuntech.health.Commodity');
-    let results = await query('selectCommoditiesWithHighQuantity');
+//     let assetRegistry = await getAssetRegistry('org.xuyuntech.health.Commodity');
+//     let results = await query('selectCommoditiesWithHighQuantity');
 
-    for (let n = 0; n < results.length; n++) {
-        let trade = results[n];
+//     for (let n = 0; n < results.length; n++) {
+//         let trade = results[n];
 
-        // emit a notification that a trade was removed
-        let removeNotification = getFactory().newEvent('org.xuyuntech.health','RemoveNotification');
-        removeNotification.commodity = trade;
-        emit(removeNotification);
-        await assetRegistry.remove(trade);
-    }
-}
+//         // emit a notification that a trade was removed
+//         let removeNotification = getFactory().newEvent('org.xuyuntech.health','RemoveNotification');
+//         removeNotification.commodity = trade;
+//         emit(removeNotification);
+//         await assetRegistry.remove(trade);
+//     }
+// }
