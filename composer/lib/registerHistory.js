6e4f3397cc8618662fe715d9b46c30c84dfbeec7
windghoul <windghoul@gmail.com>
2018-07-06 17:37:36 +0800

    lib js文件完成

--- composer/lib/registerHistory.js
+++ composer/lib/registerHistory.js
@@ -31,14 +31,39 @@ async function verifyRegisterHistoryAction(tx){
  * @transaction
  */
 async function finishRegisterHistoryAction(tx){
+  var factory = getFactory();
+  var NS = 'org.xuyuntech.health';
+  var date = new Date();
   const item = tx.registerHistory;
   console.log('verifyRegisterHistoryAction:', item);
   const patient = tx.registerHistory.patient;
   // TODO 校验 state 必须为 Visiting
+  if (item.state !== 'Visiting') {
+    throw new Error('the state is not visiting');
+  }
+  //病例创建
+  // var key = patient.name + date.toLocaleDateString();
+  var CaseItem = factory.newResource(NS, 'CaseItem', tx.id);
+  CaseItem.complained = tx.complained;
+  CaseItem.diagnose = tx.diagnose;
+  CaseItem.history = tx.history;
+  CaseItem.familyHistory = tx.familyHistory;
+  CaseItem.created = tx.created;
+  let assetRegistry_CaseItem = await getAssetRegistry(NS + '.CaseItem');
+  await assetRegistry_CaseItem.addAll([CaseItem]);
+  //处方创建
+  var Prescription = factory.newResource(NS, 'Prescription', tx.id);
+  Prescription.created = tx.created;
+  Prescription.registerHistory = item;
+  Prescription.caseItem = factory.newRelationship(NS, 'CaseItem', tx.id);
+  let assetRegistry_Prescription = await getAssetRegistry(NS + '.Prescription');
+  await assetRegistry_Prescription.addAll([Prescription]);
+
   item.state = 'Finished';
-  patient.points += tx.points;
   const registry = await getAssetRegistry('org.xuyuntech.health.RegisterHistory');
   await registry.update(item);
+
+  patient.points += tx.points;
   const registry_Patient = await getParticipantRegistry('org.xuyuntech.health.Patient');
   await registry_Patient.update(patient);
 }
