ce3b3fd085ea68dfcba7902c0888cb59fd5542cb
cabernety <330785652@qq.com>
2018-07-09 17:22:05 +0800

    12

--- application/src/config.js
+++ application/src/config.js
@@ -1,27 +1,27 @@
-
+/* eslint-disable import/prefer-default-export */
 import config from 'config';
 
-let RestServerConfig;
+let restServerConfig;
 
 try {
-  RestServerConfig = Object.assign({}, config.get('restServer'));
+  restServerConfig = Object.assign({}, config.get('restServer'));
 } catch (err) {
   if (!process.env.REST_SERVER_CONFIG) {
     throw new Error('Cannot get restServer from config, the config file may not exist. Provide this file or a value for REST_SERVER_CONFIG');
   }
-  RestServerConfig = {};
+  restServerConfig = {};
 }
 
 if (process.env.REST_SERVER_CONFIG) {
   try {
     const restServerEnv = JSON.parse(process.env.REST_SERVER_CONFIG);
     // allow for them to only specify some fields
-    RestServerConfig = Object.assign(RestServerConfig, restServerEnv);
+    restServerConfig = Object.assign(restServerConfig, restServerEnv);
   } catch (err) {
     console.error('Error getting rest config from env vars, using default');
   }
 }
 
-export default {
-  RestServerConfig,
+export const RestServerConfig = {
+  ...restServerConfig,
 };
