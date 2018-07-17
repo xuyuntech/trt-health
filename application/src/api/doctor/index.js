d81bde2cd874fd3da3bd57e804e2c8e8e2ab366f
XUPTSunHui <sun_hui_1993@163.com>
2018-07-17 16:29:58 +0800

    Delete功能测试完毕

--- application/src/api/doctor/index.js
+++ application/src/api/doctor/index.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-await-in-loop */
 import express from 'express';
 import Promise from 'promise';
-import XLSX from 'xlsx';
+import xlsx from 'xlsx';
+import multer from 'multer';
+import fs from 'fs';
 import { bfetch } from '../utils';
 import { API } from '../../const';
 import initData from './data';
 
 const router = express.Router();
+const upload = multer({ dest: 'uploads/' });
 
 router.post('/init', async (req, res) => {
   const { data } = initData;
@@ -123,12 +126,32 @@ router.post('/', async (req, res) => {
   }
 });
 
-// 上传Excel，导入医生信息
-router.post('/import', async (req) => {
-  const workbook = XLSX.readFile('/home/sunhui/trt-health/application/src/api/doctor/Doctor.xlsx');
+// 本地上传 Excel，导入医生信息
+// router.post('/import', async (req) => {
+// const workbook = xlsx.readFile('/home/sunhui/trt-health/application/src/api/doctor/Doctor.xlsx');
+//   const sheetNameList = workbook.SheetNames;
+//   const worksheet = workbook.Sheets[sheetNameList[0]];
+//   const DoctorArray = xlsx.utils.sheet_to_json(worksheet);
+
+//   for (let i = 0; i < DoctorArray.length; i += 1) {
+//     DoctorArray[i].$class = 'org.xuyuntech.health.Doctor';
+//     const body = DoctorArray[i];
+
+//     await bfetch(API.Doctor.Create(), {
+//       method: 'POST',
+//       req,
+//       body,
+//     });
+//   }
+// });
+
+
+// 使用 multer 上传文件，读取、导入 excel，
+router.post('/import', upload.single('excel'), async (req, res) => {
+  const workbook = xlsx.readFile(req.file.path);
   const sheetNameList = workbook.SheetNames;
   const worksheet = workbook.Sheets[sheetNameList[0]];
-  const DoctorArray = XLSX.utils.sheet_to_json(worksheet);
+  const DoctorArray = xlsx.utils.sheet_to_json(worksheet);
 
   for (let i = 0; i < DoctorArray.length; i += 1) {
     DoctorArray[i].$class = 'org.xuyuntech.health.Doctor';
@@ -140,6 +163,15 @@ router.post('/import', async (req) => {
       body,
     });
   }
+
+  res.json({
+    status: 0,
+    results: 'success',
+  });
+
+  // 处理后删除文件
+  fs.unlinkSync(req.file.path);
+  res.send('finished');
 });
 
 export default router;
